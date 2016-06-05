14f97042383c4031d03957e2431b4cfd14f0a7ed
simicdusan <dusan.simic1810@gmail.com>
2016-06-05 12:45:55 +0200

    changed prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,20 +1,20 @@
 function fish_prompt
   if [ (whoami) = "root" ]
     set_color normal
-    echo -n (whoami)'@'(hostname)' '
+    printf '\u250C'(whoami)'@'(hostname)' '
     set_color red
-    echo -n (prompt_pwd)
+    printf (prompt_pwd)
     set_color normal
     #echo -n ' λ '
-    printf '\nλ '
+    printf '\n\u2514λ '
   end
   if [ (whoami) = "dusan" ]
     set_color normal
-    echo -n (whoami)'@'(hostname)' '
+    printf '\u250C'(whoami)'@'(hostname)' '
     set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+    printf (prompt_pwd)
     set_color normal
     #echo -n ' λ '
-    printf '\nλ '
+    printf '\n\u2514λ '
   end
 end
