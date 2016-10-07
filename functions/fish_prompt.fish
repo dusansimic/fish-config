601dea2090efb97949095d4e53b5fbaa19f371f5
Dušan Simić <dusan.simic1810@gmail.com>
2016-10-07 21:42:10 +0200

    changed prompt a bit

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,20 +1,31 @@
 function fish_prompt
   if [ (whoami) = "root" ]
     set_color normal
-    printf '\u250C'(whoami)'@'(hostname)' '
+    #printf (whoami)'@'(hostname)' '
+    printf 'root '
     set_color red
     printf (prompt_pwd)
     set_color normal
-    #echo -n ' λ '
-    printf '\n\u2514λ '
+    #printf ' →  '
+    #printf ' \u2192 '
+    #printf ' \u25B7 '
+    #printf ' λ '
+    printf ' # '
   end
   if [ (whoami) = "dusan" ]
     set_color normal
-    printf '\u250C'(whoami)'@'(hostname)' '
+    #printf (whoami)'@'(hostname)' '
+    #printf 'ドゥシャン ' #@'(hostname)' '
+    printf '杜尚 '
     set_color $fish_color_cwd
+    #printf (basename $PWD) "><> "
     printf (prompt_pwd)
     set_color normal
-    #echo -n ' λ '
-    printf '\n\u2514λ '
+    #printf ' →  '
+    #printf ' \u2192 ' #tanka strelica
+    #printf ' \u25B7 ' #trougao
+    #printf ' λ '
+    #printf ' $ '
+    printf ' \u279E  ' #debela strelica
   end
 end
