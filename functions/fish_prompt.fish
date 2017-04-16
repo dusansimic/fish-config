8c654633ddaada2c523cbb8c2a2603509cf16991
Dušan Simić <dusan.simic1810@gmail.com>
2017-04-16 14:57:55 +0200

    edited prompt and added alias

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,31 +1,30 @@
 function fish_prompt
   if [ (whoami) = "root" ]
     set_color normal
-    #printf (whoami)'@'(hostname)' '
-    printf 'root '
+    printf (whoami)'@'(hostname)' '
+    #printf 'root '
     set_color red
     printf (prompt_pwd)
     set_color normal
     #printf ' →  '
     #printf ' \u2192 '
-    #printf ' \u25B7 '
+    printf ' \u25B7 '
     #printf ' λ '
-    printf ' # '
-  end
-  if [ (whoami) = "dusan" ]
+    #printf ' # '
+  else
     set_color normal
-    #printf (whoami)'@'(hostname)' '
+    printf (whoami)'@'(hostname)' '
     #printf 'ドゥシャン ' #@'(hostname)' '
-    printf '杜尚 '
+    #printf '杜尚 '
     set_color $fish_color_cwd
     #printf (basename $PWD) "><> "
     printf (prompt_pwd)
     set_color normal
     #printf ' →  '
     #printf ' \u2192 ' #tanka strelica
-    #printf ' \u25B7 ' #trougao
+    printf ' \u25B7 ' #trougao
     #printf ' λ '
     #printf ' $ '
-    printf ' \u279E  ' #debela strelica
+    #printf ' \u279E  ' #debela strelica
   end
 end
