f2d17e72933d48716c4a8f2f5dce45a820dd9cf0
Dušan Simić <dusan.simic1810@gmail.com>
2017-07-22 15:38:44 +0200

    cleaned up file, added git branch prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,30 +1,27 @@
 function fish_prompt
+	set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
   if [ (whoami) = "root" ]
     set_color normal
     printf (whoami)'@'(hostname)' '
-    #printf 'root '
     set_color red
     printf (prompt_pwd)
+		if [ $git_branch ]
+		  set_color yellow
+		  printf ' ('"$git_branch"')'
+    end
     set_color normal
-    #printf ' →  '
-    #printf ' \u2192 '
-    printf ' \u25B7 '
-    #printf ' λ '
-    #printf ' # '
+    printf ' λ '
   else
     set_color normal
     printf (whoami)'@'(hostname)' '
-    #printf 'ドゥシャン ' #@'(hostname)' '
-    #printf '杜尚 '
     set_color $fish_color_cwd
     #printf (basename $PWD) "><> "
     printf (prompt_pwd)
+    if [ $git_branch ]
+		  set_color yellow
+		  printf ' ('"$git_branch"')'
+    end
     set_color normal
-    #printf ' →  '
-    #printf ' \u2192 ' #tanka strelica
-    printf ' \u25B7 ' #trougao
-    #printf ' λ '
-    #printf ' $ '
-    #printf ' \u279E  ' #debela strelica
+    printf ' λ '
   end
 end
